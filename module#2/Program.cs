c3b9aec209e4e876c10b214cb1c57790680d89b1
Andrii Pasternak <a.pasternak@nltu.lviv.ua>
2023-12-19 19:22:36 +0200

    Added new project

---
+++ module#2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace module_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is module work #2 by Andrii Pasternak from KN-31/3");
+        }
+    }
+}
